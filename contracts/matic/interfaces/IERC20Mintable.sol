be12c424e883c7d15bc79ba6dcd9e09a3123602b
mkdir <projectdevelopinginbox@gmail.com>
2021-11-06 15:42:59 -0500

    more changes

--- contracts/matic/interfaces/IERC20Mintable.sol
+++ contracts/matic/interfaces/IERC20Mintable.sol
@@ -2,4 +2,5 @@ pragma solidity >=0.6.0 <0.8.0;
 
 interface IERC20Mintable {
     function mint(address _to, uint256 _amount) external returns (uint256);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
